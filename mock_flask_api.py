5a52f73e91597861adfb6c286efbb6bb6d238021
nhnminh <nhnminh@apcs.vn>
2020-11-17 23:42:57 +0100

    first commit

---
+++ mock_flask_api.py
@@ -0,0 +1,22 @@
+from flask import Flask, Response, stream_with_context
+import time
+import uuid
+import random
+
+APP = Flask(__name__)
+
+@APP.route("/large_request/<int:rowCount>", methods=["GET"])
+def get_large_request(rowCount):
+    """return N rows of data"""
+    def f():
+        """" Generator of pseudo data """
+        for _i in range(rowCount):
+            time.sleep(0.1)
+            txid = uuid.uuid4()
+            uid = uuid.uuid4()
+            amount = round(random.uniform(-1000,1000), 2)
+            yield f"('{txid}', '{uid}', '{amount}')\n"
+    return Response(stream_with_context(f()))
+
+if __name__ == "__main__":
+    APP.run(debug = True)
